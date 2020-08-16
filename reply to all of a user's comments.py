5831908f400f32f2400861f42d599f59caedc620
redditbotsthrowaway <69697651+redditbotsthrowaway@users.noreply.github.com>
2020-08-15 22:40:15 -0500

    Update reply to all of a user's comments.py

--- reply to all of a user's comments.py
+++ reply to all of a user's comments.py
@@ -1,3 +1,9 @@
+# There's a chance you could get banned/shadowed with this bot, but it also may just be a result of not sleeping 
+# long enough between comments. 
+# It can also be configured to reply to only the newest comment, if a user doesn't comment often. 
+# I will likely release more updated versions intended to respond if a keyword is triggered and save responded-to posts later,
+# but as of now, this is only meant as a basic structural function 
+
 import praw
 import random
 from time import sleep
